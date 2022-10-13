d6413a4b10564d79e37e96ef048f6aa7e88490c9
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-10-13 15:06:46 +0100

    No of_nat in sqrt

--- src/sqrt.lean
+++ src/sqrt.lean
@@ -5,10 +5,10 @@ import port_real
 
 open real
 
-lemma sqrt_atom.feasibility (x : ℝ) (cond : (of_nat 0) ≤ x) :
-  (of_nat 0) ≤ (of_nat 1) / (of_nat 2) ∧
-  sqrt x ^ 2 ≤ (of_nat 1) / (of_nat 2) * x * (of_nat 2) ∧
-  (of_nat 0) ≤ x :=
+lemma sqrt_atom.feasibility (x : ℝ) (cond : 0 ≤ x) :
+  (0 : ℝ) ≤ 0.5 ∧
+  sqrt x ^ 2 ≤ 0.5 * x * 2 ∧
+  0 ≤ x :=
 by simp [of_nat] at *; simp [sq_sqrt cond, cond]; ring_nf
 
 lemma sqrt_atom.optimality (x t : ℝ) (c2 : 0 ≤ t)
